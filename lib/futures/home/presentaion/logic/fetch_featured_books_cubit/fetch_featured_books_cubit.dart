f5f419b29c7cd4aa2765551dc4e302f591ac1e0b
TarekHayan <tarekhayan77@gmail.com>
2025-11-25 13:29:33 +0200

    refactor : refactor fetch books cubit

--- lib/futures/home/presentaion/logic/fetch_featured_books_cubit/fetch_featured_books_cubit.dart
+++ lib/futures/home/presentaion/logic/fetch_featured_books_cubit/fetch_featured_books_cubit.dart
@@ -1,18 +1,17 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/futures/home/data/Models/book_model/ook_model.dart';
-import 'package:bookly_app/futures/home/data/Repos/home_page_repo_impel.dart';
+import 'package:bookly_app/futures/home/data/Repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'fetch_featured_books_state.dart';
 
 class FetchFeaturedBooksCubit extends Cubit<FetchFeaturedBooksState> {
-  FetchFeaturedBooksCubit(this.homePageRepoImpel)
-    : super(FetchFeaturedBooksInitial());
-  final HomePageRepoImpel homePageRepoImpel;
+  FetchFeaturedBooksCubit(this.homeRepo) : super(FetchFeaturedBooksInitial());
+  final HomeRepo homeRepo;
 
   Future<void> fetchFuturedBooks() async {
     emit(FetchFeaturedBooksLoading());
-    var result = await homePageRepoImpel.fetchFeatureBooks();
+    var result = await homeRepo.fetchFeatureBooks();
     result.fold(
       (fauliarError) {
         emit(FetchFeaturedBooksFauliar(errorMsg: fauliarError.errorMsg));
